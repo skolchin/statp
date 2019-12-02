6ae9bdd7b5338f2418a07562817afb7933d5d72c
kol <skolchin@gmail.com>
2019-12-02 22:20:55 +0300

    new function: remove_outliers

---
+++ R/rmout.R
@@ -0,0 +1,73 @@
+##'
+##' Statistical data analysis support functions
+##' Sergey "kol" Kolchin, 2019
+##'
+##' remove_outliers function
+##'
+
+
+#' Remove outliers
+#'
+#' The function takes a dataset, checks that given variables for outliers and
+#' either removes them or approximate using other data in these variables.
+#'
+#' @param .data dataset (data.frame)
+#' @param vars variables to inspect (character string, formula or vector).
+#'             If omitted, all numeric columns are processed. If a single variable is provided
+#'             and approx is TRUE, outlier values are removed from dataset. If it is a vector,
+#'             data is not removed but replaced with NAs.
+#' @param approx If TRUE, outliers are approximated, if FALSE (default) - removed or replaced by NAs.
+#'               Currently not supported, reserved for further use
+#'
+#' @return resulting data.frame or a vector if source dataset contain only one column
+#'
+#' @examples
+#' \dontrun{
+#' d <- data.frame(
+#'   id = c("1","2","3","4","5"),
+#'   a = c(1,2,10,1000,100),
+#'   b = c(19999, 30000000, 1, 5, 426798))
+#' remove_outliers(d, ~a)
+#' remove_outliers(d)
+#' }
+#'
+#' @name remove_outliers
+#' @export
+remove_outliers <- function(.data, vars = NULL, approx = FALSE) {
+
+  if (!missing(vars) && rlang::is_formula(vars))
+    # Check if column names provided as formulas (~total)
+    vars <- all.vars(vars)[1]
+  else if (missing(vars)) {
+    # Find all numeric/logical values
+    cols <- sapply(names(.data), function(x) class(.data[[x]]))
+    num_cols <- cols[ which(!is.na(match(cols, c("numeric", "logical")))) ]
+    vars <- names(num_cols)
+    if (length(vars) == 0)
+      stop("No value columns identified")
+  }
+  else {
+    # Check all variables exist in dataset
+    if (length(setdiff(vars, names(.data)))>0)
+      stop(paste0("Variables missing from source data: ", setdiff(vars, names(.data))))
+  }
+
+  ds <- NULL
+  if (length(vars) == 1) {
+    # Simple case - remove outliers of one variable
+    out <- grDevices::boxplot.stats(.data[[vars[1]]])$out
+    ds <- .data[ which(!(.data[[vars[1]]] %in% out)), ]
+  }
+  else {
+    ds <- as.data.frame(
+      sapply(vars, function(v) {
+        d <- .data[[v]]
+        out <- grDevices::boxplot.stats(d)$out
+        d[ which(.data[[v]] %in% out) ] <- NA
+        d
+      }))
+    ds <- cbind(.data[setdiff(names(.data), vars)], ds)
+  }
+  return(ds)
+}
+
